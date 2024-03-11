011ec2a69dd0257ff660467b1d9c24ef5418af83
luizfonseca830 <luifonseca830@gmail.com>
2024-03-11 13:01:04 -0300

    refinada na categoria

--- src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -4,10 +4,8 @@ import com.algaworks.algamoney.api.model.Categoria;
 import com.algaworks.algamoney.api.service.CategoriaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,18 +24,12 @@ public class CategoriaResource {
     }
 
     @GetMapping("/{codigo}")
-    public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+    public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
         return categoriaService.buscarPeloCodigo(codigo);
     }
 
     @PostMapping
     public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
-        Categoria categoriaSalva = categoriaService.criar(categoria);
-
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
-        response.setHeader("Location", uri.toASCIIString());
-
-        return ResponseEntity.created(uri).body(categoriaSalva);
+        return categoriaService.criar(categoria);
     }
 }
--- src/main/java/com/algaworks/algamoney/api/service/CategoriaService.java
+++ src/main/java/com/algaworks/algamoney/api/service/CategoriaService.java
@@ -2,8 +2,12 @@ package com.algaworks.algamoney.api.service;
 
 import com.algaworks.algamoney.api.model.Categoria;
 import com.algaworks.algamoney.api.repository.CategoriaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @Service
@@ -18,12 +22,18 @@ public class CategoriaService {
         return categoriaRepository.findAll();
     }
 
-    public Categoria criar(Categoria categoria){
+    public ResponseEntity<Categoria> criar(Categoria categoria){
         Categoria categoriaSalva = categoriaRepository.save(categoria);
-        return categoriaSalva;
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        return ResponseEntity.created(uri).body(categoriaSalva);
     }
 
-    public Categoria buscarPeloCodigo(Long codigo){
-        return categoriaRepository.findById(codigo).orElse(null);
+    public ResponseEntity<Categoria> buscarPeloCodigo(Long codigo){
+        Categoria categoria = categoriaRepository.findById(codigo).orElse(null);
+        if (categoria == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(categoria);
     }
 }
